3ea0937db411e4cf6e6258fe61350d7b549f83f6
Sandip-2006 <sandip97316@gmail.com>
2025-07-31 16:17:46 +0530

    Upload X.com clone project

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*.html"],
+  theme: {
+    extend: {
+      colors:{
+        bordergray:'rgb(239,243,244)',
+        borderSearch:'rgb(213,218,219)',
+        borderSicon:'rgb(83,100,113)',
+        hoverBtn:'rgb(22,137,240)',
+        btnColor:'rgb(29,115,240)',
+        itemTCol:'rgb(83,100,113)',
+        sandip:'#410c91',
+        postBtn:'rgb(119,124,128)'
+      }
+    },
+  },
+  plugins: [],
+}
+
